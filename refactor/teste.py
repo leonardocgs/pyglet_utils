474bde8078e80af7cabd7fc592cb9730c38974cd
Leonardo Cesar <leonardocesargoes@gmail.com>
2022-10-18 12:41:20 -0300

    chore: add new domino images

--- refactor/teste.py
+++ refactor/teste.py
@@ -6,6 +6,8 @@ import os
 window = pyglet.window.Window()
 go = game_object.GameObject(img_path = "01.png",position =  vector2d.Vector2d( window.width//2, window.height//2))
 
+
+
 @window.event
 def on_draw():
     window.clear()
