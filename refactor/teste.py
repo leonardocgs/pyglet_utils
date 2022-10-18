9d2ebd06ea57a1cd10ff91610db1850c5c38da0b
Leonardo Cesar <leonardocesargoes@gmail.com>
2022-10-18 12:24:39 -0300

    refact: insert old packages into modules

--- refactor/teste.py
+++ refactor/teste.py
@@ -4,7 +4,7 @@ import pyglet
 import os
 
 window = pyglet.window.Window()
-go = game_object.GameObject(img_path = "00.png",position =  vector2d.Vector2d( window.width//2, window.height//2))
+go = game_object.GameObject(img_path = "01.png",position =  vector2d.Vector2d( window.width//2, window.height//2))
 
 @window.event
 def on_draw():
